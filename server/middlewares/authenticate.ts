a0b10f6c3e09a6729f4d3de9c3c0297535c25d09
BlacKlyExactly <supreme24d@gmail.com>
2023-03-29 21:28:36 +0200

    Fix: Authorization headers for files requests

--- server/middlewares/authenticate.ts
+++ server/middlewares/authenticate.ts
@@ -7,14 +7,14 @@ const authenticate = (req: Request, res: Response, next: NextFunction) => {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
 
-  if (!token) return res.sendStatus(401);
+  if (!token) return res.status(401).send({ error: "Unauthorized" });
 
   jwt.verify(
     token,
     process.env.COOKIE_SECRET as string,
     async (err: any, jwtUser: any) => {
       if (err) return res.status(403).send(err);
-      if (!jwtUser) return res.status(404);
+      if (!jwtUser) return res.status(404).send({ error: "User not found" });
 
       const { username } = jwtUser as User;
 
@@ -24,7 +24,7 @@ const authenticate = (req: Request, res: Response, next: NextFunction) => {
         },
       });
 
-      if (!user) return res.sendStatus(404);
+      if (!user) return res.status(404).send({ error: "User not found" });
 
       res.locals.user = username;
       next();
--- src/hooks/useFiles.ts
+++ src/hooks/useFiles.ts
@@ -1,83 +1,6 @@
 import { useEffect, useState } from "react";
 import { File } from "@prisma/client";
-
-export const fetchFiles = async (path = "/") => {
-  const request = await fetch(
-    `/api/files${path.startsWith("/") ? path : `/${path}`}`
-  );
-
-  return (await request.json()) as File[];
-};
-
-export const fileExist = async (path = "/") => {
-  const request = await fetch(
-    `/api/file-exist${path.startsWith("/") ? path : `/${path}`}`
-  );
-
-  return (await request.json()) as File;
-};
-
-export const directoryExist = async (path = "/") => {
-  const request = await fetch(
-    `/api/directory-exist${path.startsWith("/") ? path : `/${path}`}`
-  );
-
-  return (await request.json()) as File;
-};
-
-export const createFile = async (path = "/") => {
-  const request = await fetch(
-    `/api/create-file${path.startsWith("/") ? path : `/${path}`}`,
-    { method: "POST" }
-  );
-
-  return (await request.json()) as object;
-};
-
-export const createDirectory = async (path = "/") => {
-  const request = await fetch(
-    `/api/create-directory${path.startsWith("/") ? path : `/${path}`}`,
-    { method: "POST" }
-  );
-
-  return (await request.json()) as object;
-};
-
-export const removeFile = async (path = "/") => {
-  const request = await fetch(
-    `/api/remove-file${path.startsWith("/") ? path : `/${path}`}`,
-    { method: "DELETE" }
-  );
-
-  return (await request.json()) as object;
-};
-
-export const removeDirectory = async (path = "/") => {
-  const request = await fetch(
-    `/api/remove-directory${path.startsWith("/") ? path : `/${path}`}`,
-    { method: "DELETE" }
-  );
-
-  return (await request.json()) as object;
-};
-
-export const modifyFile = async (path = "/", content: string = "") => {
-  const request = await fetch(
-    `/api/modify-file${path.startsWith("/") ? path : `/${path}`}`,
-    {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json",
-      },
-      method: "PATCH",
-      body: JSON.stringify({
-        content,
-      }),
-    }
-  );
-
-  return (await request.json()) as object;
-};
+import Cookies from "js-cookie";
 
 const useFiles = (path = "/") => {
   const [files, setFiles] = useState<File[]>([]);
@@ -92,6 +15,120 @@ const useFiles = (path = "/") => {
   }, []);
 
   return { files, refetchFiles };
+};
+
+export const fetchFiles = async (path = "/") => {
+  const request = await fetch(
+    `/api/files${path.startsWith("/") ? path : `/${path}`}`,
+    {
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+    }
+  );
+
+  return (await request.json()) as File[];
+};
+
+export const fileExist = async (path = "/") => {
+  const request = await fetch(
+    `/api/file-exist${path.startsWith("/") ? path : `/${path}`}`,
+    {
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+    }
+  );
+
+  return (await request.json()) as File;
+};
+
+export const directoryExist = async (path = "/") => {
+  const request = await fetch(
+    `/api/directory-exist${path.startsWith("/") ? path : `/${path}`}`,
+    {
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+    }
+  );
+
+  return (await request.json()) as File;
+};
+
+export const createFile = async (path = "/") => {
+  const request = await fetch(
+    `/api/create-file${path.startsWith("/") ? path : `/${path}`}`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+    }
+  );
+
+  return (await request.json()) as object;
+};
+
+export const createDirectory = async (path = "/") => {
+  const request = await fetch(
+    `/api/create-directory${path.startsWith("/") ? path : `/${path}`}`,
+    {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+    }
+  );
+
+  return (await request.json()) as object;
+};
+
+export const removeFile = async (path = "/") => {
+  const request = await fetch(
+    `/api/remove-file${path.startsWith("/") ? path : `/${path}`}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+    }
+  );
+
+  return (await request.json()) as object;
+};
+
+export const removeDirectory = async (path = "/") => {
+  const request = await fetch(
+    `/api/remove-directory${path.startsWith("/") ? path : `/${path}`}`,
+    {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+    }
+  );
+
+  return (await request.json()) as object;
+};
+
+export const modifyFile = async (path = "/", content: string = "") => {
+  const request = await fetch(
+    `/api/modify-file${path.startsWith("/") ? path : `/${path}`}`,
+    {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+      method: "PATCH",
+      body: JSON.stringify({
+        content,
+      }),
+    }
+  );
+
+  return (await request.json()) as object;
 };
 
 export default useFiles;
